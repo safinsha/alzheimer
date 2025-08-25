93320cb1a2669b2f7ff67c6c50065eedf09fdee1
safin.lewagon@gmail.com <safin.lewagon@gmail.com>
2025-08-25 15:31:03 +0000

    table intermediate safin

--- models/staging/alz/stg_alz__raw_prediction.sql
+++ models/staging/alz/stg_alz__raw_prediction.sql
@@ -17,10 +17,10 @@ renamed as (
         physical_activity_level,
         smoking_status,
         alcohol_consumption,
-        diabetes,
-        hypertension,
+        CASE WHEN diabetes = false THEN 0 ELSE 1 END as diabetes,
+        CASE WHEN hypertension = false THEN 0 ELSE 1 END as hypertension,
         cholesterol_level,
-        fam_history,
+        CASE WHEN fam_history = false THEN 0 ELSE 1 END as fam_history,
         cognitive_test_score,
         depression_level,
         sleep_quality,
@@ -28,12 +28,12 @@ renamed as (
         air_pollution_exposure,
         employment_status,
         marital_status,
-        genetic_risk_factor_apoe_e4_allele,
+        CASE WHEN Genetic_Risk_Factor_APOE_e4_allele = false THEN 0 ELSE 1 END as Genetic_Risk_Factor_APOE_e4_allele,
         social_engagement_level,
         income_level,
         stress_levels,
         `urban_vs_rural living` as urban_vs_rural,
-        alzheimer_diagnosis
+        CASE WHEN Alzheimer_Diagnosis = false THEN 0 ELSE 1 END as Alzheimer_Diagnosis
 
     from source
 
